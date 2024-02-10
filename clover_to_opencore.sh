794420523b905278260a296a524a7589f8ba0e71
DigitalBeatsFi <33343969+DigitalBeatsFi@users.noreply.github.com>
2024-02-10 13:54:21 +0200

    clover_to_opencore.sh

--- clover_to_opencore.sh
+++ clover_to_opencore.sh
@@ -15,24 +15,41 @@ opencore_file="$2"
 # Luodaan uusi tiedosto OpenCorelle
 output_file="opencore_config.plist"
 
-# Muunnetaan Cloverin KernelAndKextPatches-asetukset OpenCorelle
+# Muunnetaan Cloverin ACPI-asetukset OpenCorelle
+sed -n '/<key>ACPI<\/key>/,/<\/dict>/p' "$clover_file" > "$output_file"
+
+# Lisätään uudelle riville "ACPI" tekstin tilalle "ACPI:"
+sed -i '' 's/<key>ACPI<\/key>/<key>ACPI:<\/key>/g' "$output_file"
+
+# Lisätään OpenCoren ACPI-asetukset Clover-tiedoston perään
+sed -n '/<key>Booter<\/key>/,/<\/dict>/p' "$clover_file" >> "$output_file"
+
+# Muunnetaan Cloverin DeviceProperties-asetukset OpenCorelle
+sed -n '/<key>Devices<\/key>/,/<\/dict>/p' "$clover_file" > "$output_file"
+
+# Lisätään uudelle riville "DeviceProperties" tekstin tilalle "DeviceProperties:"
+sed -i '' 's/<key>Devices<\/key>/<key>Devices:<\/key>/g' "$output_file"
+
+# Lisätään OpenCoren DeviceProperties-asetukset Clover-tiedoston perään
+sed -n '/<key>Boot<\/key>/,/<\/dict>/p' "$clover_file" >> "$output_file"
+
+# Muunnetaan Cloverin Kernel-asetukset OpenCorelle
 sed -n '/<key>KernelAndKextPatches<\/key>/,/<\/dict>/p' "$clover_file" > "$output_file"
 
-# Lisätään uudelle riville "Kernel" tekstin tilalle "Kernel:"
-sed -i '' 's/<key>KernelAndKextPatches<\/key>/<key>Kernel:<\/key>/g' "$output_file"
-
-# Lisätään uudelle riville "Enabled" tekstin tilalle "Enabled:"
-sed -i '' 's/<key>AppleRTC<\/key>/<key>AppleRTC:<\/key>/g' "$output_file"
-sed -i '' 's/<key>DellSMBIOSPatch<\/key>/<key>DellSMBIOSPatch:<\/key>/g' "$output_file"
-sed -i '' 's/<key>ForceKextsToLoad<\/key>/<key>ForceKextsToLoad:<\/key>/g' "$output_file"
-sed -i '' 's/<key>KernelLapic<\/key>/<key>KernelLapic:<\/key>/g' "$output_file"
-sed -i '' 's/<key>KernelPm<\/key>/<key>KernelPm:<\/key>/g' "$output_file"
-
-# Lisätään uudelle riville "KernelToPatch" tekstin tilalle "KernelToPatch:"
-sed -i '' 's/<key>KernelToPatch<\/key>/<key>KernelToPatch:<\/key>/g' "$output_file"
+# Lisätään uudelle riville "KernelAndKextPatches" tekstin tilalle "KernelAndKextPatches:"
+sed -i '' 's/<key>KernelAndKextPatches<\/key>/<key>KernelAndKextPatches:<\/key>/g' "$output_file"
 
 # Lisätään OpenCoren Kernel-asetukset Clover-tiedoston perään
 sed -n '/<key>Boot<\/key>/,/<\/dict>/p' "$clover_file" >> "$output_file"
 
+# Muunnetaan Cloverin Misc-asetukset OpenCorelle
+sed -n '/<key>Misc<\/key>/,/<\/dict>/p' "$clover_file" > "$output_file"
+
+# Lisätään uudelle riville "Misc" tekstin tilalle "Misc:"
+sed -i '' 's/<key>Misc<\/key>/<key>Misc:<\/key>/g' "$output_file"
+
+# Lisätään OpenCoren Misc-asetukset Clover-tiedoston perään
+sed -n '/<key>Boot<\/key>/,/<\/dict>/p' "$clover_file" >> "$output_file"
+
 # Tulostetaan muunnos onnistuneesti
-echo "KernelAndKextPatches-asetukset on muunnettu onnistuneesti tiedostosta $clover_file tiedostoon $output_file OpenCorelle."
+echo "Cloverista OpenCoreen muunnos suoritettu. Tulostettu tiedostoon: $output_file."
